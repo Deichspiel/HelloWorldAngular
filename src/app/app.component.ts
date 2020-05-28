74acfdd795c4f59f0fe290a515ec2bf3f15e01b7
Deichspiel <Deichspiel@mailbox.org>
2020-05-28 22:30:31 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angularTest';
+
+  todoItem: string;
+  toDoArray:Array<string>=[];
+ 
+  addToDo(){
+    this.toDoArray.push(this.todoItem);
+    this.todoItem = '';
+  }
 }
